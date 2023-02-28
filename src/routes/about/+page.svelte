1dacbcf07423c506d67eaedd9df0d49a86be8a54
colecody27 <colecody27@gmailcom>
2023-02-28 13:58:57 -0800

    Ran commands once more

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -25,23 +25,22 @@
 </body>
 
 <style>
-    .blob{
-        display:flex;
-        margin: auto;
-        margin-top: 10vh;
-        color: black;
-        font-size: 25px;
-    }
-    .diagram{
-        display:inline-block;
-        border-radius: 10px;
-        background-color: white;
-        width: 100%;
-        max-width: 600px;
-        height: 25vh;
-  
-    }
-    body {
+	.blob {
+		display: flex;
+		margin: auto;
+		margin-top: 10vh;
+		color: black;
+		font-size: 25px;
+	}
+	.diagram {
+		display: inline-block;
+		border-radius: 10px;
+		background-color: white;
+		width: 100%;
+		max-width: 600px;
+		height: 25vh;
+	}
+	body {
 		background-image: url(https://img.freepik.com/free-vector/gradient-liquid-abstract-background_23-2148914626.jpg?w=996&t=st=1677614748~exp=1677615348~hmac=b85e5627e48713d263e834ab6c2b27ae7342d04d0c5a5d2ab5c4ff0e5b6ef2b7);
 		background-repeat: no-repeat;
 		background-size: cover;
