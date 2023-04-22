4ee44b80d1d9b5f857dd4463e66dff5f7d2d3229
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-04-22 16:08:24 -0700

    Remove unused env variable `GITHUB_OAUTH_URL.

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -9,8 +9,3 @@ export const DEV_FLAG_ENABLED = parseBoolean(STRING_DEV_FLAG_ENABLED);
 export const GITHUB_PROD_CLIENT = STRING_GITHUB_PROD_CLIENT;
 export const GITHUB_DEV_CLIENT = STRING_GITHUB_DEV_CLIENT;
 export const GITHUB_CLIENT = DEV_FLAG_ENABLED ? GITHUB_DEV_CLIENT : GITHUB_PROD_CLIENT;
-
-const GITHUB_OAUTH_URL = new URL('https://github.com/login/oauth/authorize');
-GITHUB_OAUTH_URL.searchParams.set('client_id', GITHUB_CLIENT);
-
-export { GITHUB_OAUTH_URL };
