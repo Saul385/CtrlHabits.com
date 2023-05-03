c98ad81f4054d2476393536211ec3cec93eaa897
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-05-02 20:51:34 -0700

    Update +server.ts

--- src/routes/api/habits/+server.ts
+++ src/routes/api/habits/+server.ts
@@ -21,22 +21,31 @@ export async function POST(event: RequestEvent): Promise<Response> {
 
 	// Get the habit data from the form data.
 	const formData = await event.request.formData();
-	const addHabitRequest = makeAddHabitRequest(formData, event.locals.user.id);
-	if (addHabitRequest.error !== null) {
-		return new Response(addHabitRequest.error, { status: 400 });
+	const { error, request: addHabitRequest } = makeAddHabitRequest(formData, event.locals.user.id);
+	if (error !== null) {
+		return new Response(error, { status: 400 });
 	}
 
 	// Check if the chosen tag is available. If so, return an error.
 	const ctrlhabitsService = makeCTRLHabitsService(CTRLHABITS_SERVICE_TYPE);
-	const habit = await ctrlhabitsService.addHabit(addHabitRequest.request);
+	const habit = await ctrlhabitsService.addHabit(addHabitRequest);
 	return new Response(JSON.stringify(habit), { status: 200 });
 }
 
 /**
  * The server-side load function for:
  * `PATCH /api/habits`
+ *
+ * TODO: Implement this. Also, implement the makeUpdateHabitRequest
+ * helper function.
  */
 
+/**
+ * makeAddHabitRequest takes a FormData object and returns an AddHabitRequest
+ * object if the form data is valid. Otherwise, it returns an error message.
+ *
+ * TODO(EthanThatOneKid): Manually test this function.
+ */
 function makeAddHabitRequest(
 	formData: FormData,
 	userID: ID
@@ -56,13 +65,13 @@ function makeAddHabitRequest(
 		return { error: "Missing 'frequency' in form data", request: null };
 	}
 
-	const start_date = formData.get('start_date');
-	if (typeof start_date !== 'string') {
+	const startDate = formData.get('start_date');
+	if (typeof startDate !== 'string') {
 		return { error: "Missing 'start_date' in form data", request: null };
 	}
 
-	const end_date = formData.get('end_date');
-	if (typeof end_date !== 'string') {
+	const endDate = formData.get('end_date');
+	if (typeof endDate !== 'string') {
 		return { error: "Missing 'end_date' in form data", request: null };
 	}
 
@@ -76,8 +85,8 @@ function makeAddHabitRequest(
 			name,
 			description,
 			frequency: parseFloat(frequency),
-			start_date,
-			end_date,
+			start_date: startDate,
+			end_date: endDate,
 			private: parseBoolean(isPrivate),
 			user_id: userID
 		},
