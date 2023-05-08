36f0bf32c31331d8f45fa929156c0e869d08446d
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-05-08 10:48:35 -0700

    Add `Validated` type in `formdata.ts`

--- src/lib/server/ctrlhabits/utils/formdata.ts
+++ src/lib/server/ctrlhabits/utils/formdata.ts
@@ -1,10 +1,15 @@
 import * as z from 'zod';
-import type { AddHabitRequest, User } from '$lib/server/ctrlhabits';
+import type {
+	AddHabitRequest,
+	UpdateUserRequest,
+	UpdateHabitRequest,
+	User
+} from '$lib/server/ctrlhabits';
 
 /**
- * CTRLHabitsRequest is a type that represents a request that has been validated.
+ * Validated is a type that represents a request that has been validated.
  */
-export type CTRLHabitsRequest<T> =
+export type Validated<T> =
 	| { request: T; error: null }
 	| {
 			request: null;
@@ -12,22 +17,10 @@ export type CTRLHabitsRequest<T> =
 	  };
 
 /**
- * toAddHabitRequest takes a FormData object and returns an AddHabitRequest
- * object if the form data is valid. Otherwise, it returns a list of errors.
+ * toValidatedRequest takes a Zod schema and data and returns a Validated object.
  */
-export function toAddHabitRequest(
-	formData: FormData,
-	user: User
-): CTRLHabitsRequest<AddHabitRequest> {
-	const parsedData = addHabitRequestParser.safeParse({
-		name: formData.get('name'),
-		description: formData.get('description'),
-		frequency: formData.get('frequency'),
-		start_date: formData.get('start_date'),
-		end_date: formData.get('end_date'),
-		private: formData.get('private'),
-		user_id: user.id
-	});
+export function toValidatedRequest<T>(schema: z.ZodType<T>, data: unknown): Validated<T> {
+	const parsedData = schema.safeParse(data);
 	if (!parsedData.success) {
 		return {
 			error: parsedData.error.formErrors.fieldErrors,
@@ -42,14 +35,118 @@ export function toAddHabitRequest(
 }
 
 /**
- * addHabitRequestParser is a Zod parser for AddHabitRequest.
+ * makeRequestValidator takes a Zod schema and returns a function that takes
+ * data and returns a Validated object.
+ *
+ * A transaction must be made to validate that the user is able to make the
+ * request by checking the user's id against the user_id in the request.
  */
-export const addHabitRequestParser: z.ZodType<AddHabitRequest> = z.object({
-	name: z.string(),
-	description: z.string(),
+export function makeRequestValidator<T>(schema: z.ZodType<T>) {
+	return (data: { [k in keyof T]: unknown }): Validated<T> => toValidatedRequest(schema, data);
+}
+
+/**
+ * toUpdateUserRequest takes a FormData object and returns an UpdateUserRequest
+ * object if the form data is valid. Otherwise, it returns a list of errors.
+ */
+export function toUpdateUserRequest(formData: FormData, user: User): Validated<UpdateUserRequest> {
+	const data = Object.fromEntries(formData.entries());
+	return validateUpdateUserRequest({
+		id: user.id,
+		tag: data.tag,
+		bio: data.bio,
+		avatar_url: data.avatar_url,
+		github_id: data.github_id,
+		google_id: data.google_id
+	});
+}
+
+/**
+ * updateUserRequestSchema is a Zod schema for UpdateUserRequest.
+ */
+const updateUserRequestSchema: z.ZodType<UpdateUserRequest> = z.object({
+	id: z.string(),
+	tag: z.string().optional(),
+	bio: z.string().optional(),
+	avatar_url: z.string().url().optional(),
+	github_id: z.string().nullable().optional(),
+	google_id: z.string().nullable().optional()
+});
+
+/**
+ * validateUpdateUserRequest is a function that takes a FormData object and returns a
+ * Validated<UpdateUserRequest> object.
+ */
+export const validateUpdateUserRequest =
+	makeRequestValidator<UpdateUserRequest>(updateUserRequestSchema);
+
+/**
+ * toAddHabitRequest takes a FormData object and returns an AddHabitRequest
+ * object if the form data is valid. Otherwise, it returns a list of errors.
+ */
+export function toAddHabitRequest(formData: FormData, user: User): Validated<AddHabitRequest> {
+	const data = Object.fromEntries(formData.entries());
+	return validateAddHabitRequest({
+		user_id: user.id,
+		name: data.name,
+		description: data.description,
+		frequency: data.frequency,
+		start_date: data.start_date,
+		end_date: data.end_date,
+		private: data.private
+	});
+}
+
+/**
+ * addHabitRequestSchema is a Zod schema for AddHabitRequest.
+ */
+const addHabitRequestSchema: z.ZodType<AddHabitRequest> = z.object({
+	name: z.string().min(1).max(255),
+	description: z.string().max(255),
 	frequency: z.number(),
 	start_date: z.string().datetime(),
 	end_date: z.string().datetime(),
 	private: z.boolean(),
 	user_id: z.string()
 });
+
+/**
+ * validateAddHabitRequest is a function that takes a FormData object and returns a
+ * Validated<AddHabitRequest> object.
+ */
+export const validateAddHabitRequest = makeRequestValidator<AddHabitRequest>(addHabitRequestSchema);
+
+/**
+ * toUpdateHabitRequest takes a FormData object and returns an UpdateHabitRequest
+ */
+export function toUpdateHabitRequest(formData: FormData): Validated<UpdateHabitRequest> {
+	const data = Object.fromEntries(formData.entries());
+	return validateUpdateHabitRequest({
+		id: data.id,
+		name: data.name,
+		description: data.description,
+		frequency: data.frequency,
+		start_date: data.start_date,
+		end_date: data.end_date,
+		private: data.private
+	});
+}
+
+/**
+ * updateHabitRequestSchema is a Zod parser for UpdateHabitRequest.
+ */
+export const updateHabitRequestSchema: z.ZodType<UpdateHabitRequest> = z.object({
+	id: z.string(),
+	name: z.string().optional(),
+	description: z.string().optional(),
+	frequency: z.number().optional(),
+	start_date: z.string().datetime().optional(),
+	end_date: z.string().datetime().optional(),
+	private: z.boolean().optional()
+});
+
+/**
+ * validateUpdateHabitRequest is a Zod schema for UpdateHabitRequest.
+ */
+export const validateUpdateHabitRequest =
+	makeRequestValidator<UpdateHabitRequest>(updateHabitRequestSchema);
